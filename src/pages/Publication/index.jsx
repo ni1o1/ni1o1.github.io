77da78f5da842a07220f34b692ed7f671c69a3bb
ni1o1 <53589767+ni1o1@users.noreply.github.com>
2022-11-24 20:24:14 +0800

    update paper

--- src/pages/Publication/index.jsx
+++ src/pages/Publication/index.jsx
@@ -25,6 +25,8 @@ export default function Publication() {
   
   ## 学术论文
   
+  论文可在这里下载:[百度网盘链接](https://pan.baidu.com/s/1gx9fDyYdJPcmL5WwZOZDnA?pwd=efak) 提取码: efak 
+
   ### 2022
   1. [Sun S, Zheng Y, Dong Y, Li N, Jin Z, **Yu Q**. Reducing external container trucks’ turnaround time in ports: A data-driven approach under truck appointment systems. Computers & Industrial Engineering](https://doi.org/10.1016/j.cie.2022.108787)
   1. [Duan Z, Liu X, **Yu Q**, Li Y. Analyzing Detour Behavior of Metro Passengers Based on Mobile Phone Data. Transportation Planning and Technology](https://www.tandfonline.com/doi/abs/10.1080/03081060.2022.2103559).
@@ -49,28 +51,28 @@ export default function Publication() {
   
   ### 2020
   
-  1. [**Yu, Q.**; Li, W.; Yang, D.; Zhang, H. Mobile Phone Data in Urban Commuting: A Network Community Detection-Based Framework to Unveil the Spatial Structure of Commuting Demand. Journal of Advanced Transportation, 2020.](https://doi.org/10.1155/2020/8835981)([Download pdf](/assets/mypapers/2020JAT.pdf))
+  1. [**Yu, Q.**; Li, W.; Yang, D.; Zhang, H. Mobile Phone Data in Urban Commuting: A Network Community Detection-Based Framework to Unveil the Spatial Structure of Commuting Demand. Journal of Advanced Transportation, 2020.](https://doi.org/10.1155/2020/8835981)
   2. [**Yu, Q.**; Li, W.; Chen, J.; Yang, D.; Sui, Y.; Zhang, H. GPS data in urban taxi-sharing: A routing behavior based potential assessment method and spatiotemporal analysis on operation efficiency and energy consumption. enerarxiv 20.](https://www.enerarxiv.org/page/thesis.html?id=2416)
   3. [Jiang W, Zhang H, Long Y, Chen J, Sui Y, Song X, Shibasaki R, **Yu Q**, GPS data in urban online ride-hailing: The technical potential analysis of demand prediction model, Journal of Cleaner Production, 2020, 123706](http://www.sciencedirect.com/science/article/pii/S0959652620337513)
-  4. [**Yu, Q.**; Li, W.; Zhang, H.; Yang, D. Mobile Phone Data in Urban Customized Bus: A Network-based Hierarchical Location Selection Method with an Application to System Layout Design in the Urban Agglomeration. Sustainability 2020, 12, 6203.](https://www.mdpi.com/2071-1050/12/15/6203)([Download pdf](/assets/mypapers/customizedbus2.pdf))
-  5. [**Yu Q**, Zhang H, Li W, et al. Mobile phone GPS data in urban customized bus: dynamic line design and emission reduction potentials analysis. Journal of Cleaner Production, 2020, p. 122471.](https://www.sciencedirect.com/science/article/pii/S095965262032518X)([Download pdf](/assets/mypapers/customizedbus.pdf))
-  6. [何凌晖,**余庆**,李玮峰,李健,杨东援.新冠肺炎疫情影响下的城际交通运输需求分析.城市交通:1-11.https://doi.org/10.13813/j.cn11-5141/u.2020.0024.](http://kns.cnki.net/kcms/detail/11.5141.U.20200521.1714.002.html)([Download pdf](/assets/mypapers/新冠肺炎疫情影响下的城际交通运输需求分析.pdf))
-  7. [**Yu Q**, Zhang H, Li W, et al. Mobile phone data in urban bicycle-sharing: Market-oriented sub-area division and spatial analysis on emission reduction potentials. Journal of Cleaner Production, 2020, 254: 119974.](https://www.sciencedirect.com/science/article/pii/S0959652620300214)([Download pdf](/assets/mypapers/bicycle-sharing.pdf))
-  
+  4. [**Yu, Q.**; Li, W.; Zhang, H.; Yang, D. Mobile Phone Data in Urban Customized Bus: A Network-based Hierarchical Location Selection Method with an Application to System Layout Design in the Urban Agglomeration. Sustainability 2020, 12, 6203.](https://www.mdpi.com/2071-1050/12/15/6203)
+  5. [**Yu Q**, Zhang H, Li W, et al. Mobile phone GPS data in urban customized bus: dynamic line design and emission reduction potentials analysis. Journal of Cleaner Production, 2020, p. 122471.](https://www.sciencedirect.com/science/article/pii/S095965262032518X)
+  6. [何凌晖,**余庆**,李玮峰,李健,杨东援.新冠肺炎疫情影响下的城际交通运输需求分析.城市交通:1-11.https://doi.org/10.13813/j.cn11-5141/u.2020.0024.](http://kns.cnki.net/kcms/detail/11.5141.U.20200521.1714.002.html)
+  7. [**Yu Q**, Zhang H, Li W, et al. Mobile phone data in urban bicycle-sharing: Market-oriented sub-area division and spatial analysis on emission reduction potentials. Journal of Cleaner Production, 2020, 254: 119974.](https://www.sciencedirect.com/science/article/pii/S0959652620300214)
+
   ### 2019
   
-  1. [**Yu Q**, Li W, Yang D. Overlapping communities in network unveil urban commuting structure from mobile phone data. *Transportation Research Board 98th Annual MeetingTransportation Research Board*](http://amonline.trb.org/68387-trb-1.4353651/t005-1.4370696/1214-1.4371409/19-00578-1.4364053/19-00578-1.4371444)([Download pdf](/assets/mypapers/TRB2019.pdf))
-  
+  1. [**Yu Q**, Li W, Yang D. Overlapping communities in network unveil urban commuting structure from mobile phone data. *Transportation Research Board 98th Annual MeetingTransportation Research Board*](http://amonline.trb.org/68387-trb-1.4353651/t005-1.4370696/1214-1.4371409/19-00578-1.4364053/19-00578-1.4371444)
+
   ### 2018
   
   1. [Wang Y, Chen C, **Yu Q**. Analysis of Chinese Typical Urban Public Transport Network Types and Influencing Factors//CICTP 2018: Intelligence, Connectivity, and Mobility. Reston, VA: American Society of Civil Engineers, 2018: 913-924.](https://ascelibrary.org/doi/abs/10.1061/9780784481523.091)
-  2. [**Yu Q**, Li W, Duan Z, et al. Unveiling Urban Commuting Structure from Mobile Phone Data: A Case Study in Shanghai, China. 2018. *Transportation Research Board 97th Annual MeetingTransportation Research Board*](https://trid.trb.org/view/1494574)([Download pdf](/assets/mypapers/TRB2018.pdf))
+  2. [**Yu Q**, Li W, Duan Z, et al. Unveiling Urban Commuting Structure from Mobile Phone Data: A Case Study in Shanghai, China. 2018. *Transportation Research Board 97th Annual MeetingTransportation Research Board*](https://trid.trb.org/view/1494574)
   3. [李俊,**余庆**,杨东援.城市生活性物流设施网络实证研究.综合运输,2018,40(04):84-91.](http://cpfd.cnki.com.cn/Article/CPFDTOTAL-CSJT201706001103.htm)
   
   ### 2017
   
-  1. [**Yu Q**, Li W F, Duan Z Y, et al. Activity Space Investigations for Nonresidents Using Mobile Phone Data. *CICTP 2017: Transportation Reform and Change—Equity, Inclusiveness, Sharing, and Innovation*](https://ascelibrary.org/doi/abs/10.1061/9780784480915.015)([Download pdf](/assets/mypapers/CICTP2017.pdf))
-  2. [**余庆**, 雷曾翔, 李玮峰, 等. 基于多源数据的公交客流规模关联分析. 综合运输, 2017, 39(6): 58-63.](http://www.cnki.com.cn/Article/CJFDTotal-YSZH201706015.htm)([Download pdf](/assets/mypapers/zongheyunshu.pdf))
+  1. [**Yu Q**, Li W F, Duan Z Y, et al. Activity Space Investigations for Nonresidents Using Mobile Phone Data. *CICTP 2017: Transportation Reform and Change—Equity, Inclusiveness, Sharing, and Innovation*](https://ascelibrary.org/doi/abs/10.1061/9780784480915.015)
+  2. [**余庆**, 雷曾翔, 李玮峰, 等. 基于多源数据的公交客流规模关联分析. 综合运输, 2017, 39(6): 58-63.](http://www.cnki.com.cn/Article/CJFDTotal-YSZH201706015.htm)
   3. [李俊, 杨东援, **余庆**. 基于 POI 数据的配送企业网络布局. 2017 年中国城市交通规划年会论文集, 2017.](http://cpfd.cnki.com.cn/Article/CPFDTOTAL-CSJT201706001103.htm)
   4. [李玮峰, 程小云, **余庆**, 等. 基于多源数据的居民活动区域特征分析. 规划 60 年: 成就与挑战——2016 中国城市规划年会论文集 04 城市规划新技术应用, 2016.](http://cpfd.cnki.com.cn/Article/CPFDTOTAL-ZHCG201609004079.htm)
   
