66b1161c98580364fb0c438f2ee24bee2613dbfa
ni1o1 <53589767+ni1o1@users.noreply.github.com>
2024-11-08 21:06:55 +0800

    upup

--- src/pages/News/NewsDetail.jsx
+++ src/pages/News/NewsDetail.jsx
@@ -1,58 +1,26 @@
 // NewsDetail.js
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { HomeOutlined, UserOutlined } from '@ant-design/icons';
-import {  Breadcrumb,Divider } from 'antd';
-
-import { useNavigate } from 'react-router-dom';
 import ReactMarkdown from 'react-markdown';
-//import matter from 'front-matter';
+import axios from 'axios';
 
 export default function NewsDetail() {
-  const yamlFront = require('yaml-front-matter');
 
-
-  const navigate = useNavigate();
   const { filename } = useParams();
   const [Content, setContent] = useState({
-    attributes: {
-      title: '',
-      date: '',
-      brief: '',
-    },
-    body: '',
   });
 
   useEffect(() => {
-    const fetchMarkdown = async () => {
-      const response = await fetch(`posts/${filename}`)
-      const fileContent = await response.text();
-      const parsedContent = yamlFront.loadFront(fileContent);
-      //const parsedContent = matter(fileContent);
-      setContent(parsedContent);
-    };
-
-    fetchMarkdown();
+    axios.get(`posts/${filename}`).then(res => {
+      setContent(res.data);
+    });
   }, [filename]);
-console.log(Content)
+
   return (
     <>
-       <Breadcrumb>
-        <Breadcrumb.Item onClick={()=>{
-          navigate('/news')
-        }}>
-          <a>
-          <HomeOutlined />
-          
-          </a>
-        </Breadcrumb.Item>
-        <Breadcrumb.Item >
-          {Content.title}
-        </Breadcrumb.Item>
-      </Breadcrumb>
-      <Divider></Divider>
+ 
       <div className='markdown-body'>
-      <ReactMarkdown children={Content.__content}  />
+        <ReactMarkdown children={Content} />
       </div>
     </>
   );
--- src/pages/News/index.jsx
+++ src/pages/News/index.jsx
@@ -1,19 +1,51 @@
-import React, { useState } from 'react';
-import {  Divider, Breadcrumb } from 'antd';
-import { HomeOutlined } from '@ant-design/icons';
-import NewsList from './NewsList';
+// Introduction.js
+import React, { useEffect, useState } from 'react';
+import { Typography, List, Tag } from 'antd';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
+export default function Intro() {
 
-export default function News() {
+  const navigate = useNavigate();
   const [news, setNews] = useState([]);
 
-
+  useEffect(() => {
+    axios.get('posts/allposts.json').then(res => {
+      const files = res.data;
+      setNews(files.posts.sort((a, b) => new Date(b.date) - new Date(a.date)));
+    })
+  }, [])
 
   return (
     <>
-      
-      <NewsList />
-
+      <Typography>
+        <List
+          itemLayout="vertical"
+          size="large"
+          dataSource={news}
+          pagination={{
+            pageSize: 3,
+          }}
+          renderItem={(item, index) => (
+            <List.Item>
+              <List.Item.Meta
+                title={<a onClick={() => {
+                  navigate(`/news/${item.filename}`)
+                }}>{item.title}
+                </a>}
+                description={<>
+                  {item.tags && item.tags.map(f => (
+                    <Tag bordered={false} >
+                      {f}
+                    </Tag>))}
+                  {item.brief}
+                </>}
+              />
+              {item.date}
+            </List.Item>
+          )}
+        />
+      </Typography>
     </>
-  )
+  );
 }
